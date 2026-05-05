8ff866c76533085064db00395d2f088d20903192
Vitor Furini <vitormfurini@gmail.com>
2026-05-04 21:04:55 -0300

    Initial Commit

---
+++ wallpaper-collage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wallpaper-collage
+//
+//  Created by Vitor Furini on 04/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wallpaper-collage/wallpaper_collageApp.swift
@@ -0,0 +1,17 @@
+//
+//  wallpaper_collageApp.swift
+//  wallpaper-collage
+//
+//  Created by Vitor Furini on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct wallpaper_collageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
